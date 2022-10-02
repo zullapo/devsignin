ec76bc02a984fbac292f1e4f80218114a497f6e9
zullapo <pcluz62@gmail.com>
2022-10-02 12:17:19 -0300

    Aula 60 - Criando um app NestJS com autenticação JWT usando MongoDB e Mongoose - parte 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
